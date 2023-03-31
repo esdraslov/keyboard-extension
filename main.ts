b5da1b63cac852e4ab7ae046bd05ae6e396c1ee6
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-03-31 16:27:31 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,43 @@
 namespace KeyBoard{
-    
+    export enum Key {
+        //% block='Q'
+        Q,
+        //% block='W'
+        W,
+        //% block='E'
+        E,
+        //% block='R'
+        R,
+        //% block='T'
+        T,
+        //% block='Y'
+        Y,
+        //% block='U'
+        U,
+        //% block='I'
+        I,
+        //% block='O'
+        O,
+        //% block='P'
+        P,
+        //% block='A'
+        A,
+        //% block='S'
+        S
+    }
+    export enum Event {
+        //% block='key up'
+        KeyUp,
+        //% block='key down'
+        KeyDown
+    }
+    export enum KeyBoardState {
+        //% block='up'
+        Up,
+        //% block='down'
+        Down
+    }
+    export function setKeyBoard(state: KeyBoardState){
+
+    }
 }
